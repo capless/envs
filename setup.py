57dd09af24ba266a3de78f178285cf0cf373b8e7
Brian Jinwright <bjinwright@qwigo.com>
2017-05-04 15:47:01 -0400

    fixed typo in MANIFEST, removed setup.cfg, and updated version to 1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=[str(ir.req) for ir in install_reqs],
     packages=find_packages(),
     py_modules=['envs.cli'],
-    version='1.0',
+    version='1.1',
     entry_points='''
         [console_scripts]
         envs=envs.cli:envs
