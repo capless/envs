b20c64d27671b99d4c9b25de099f1cad985fc227
Brian Jinwright <brian@ipoots.com>
2018-06-05 09:51:54 -0400

    fixed pip 10 issue

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
-from pip.req import parse_requirements
 
-cli_reqs = parse_requirements('requirements_cli.txt', session=False)
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
 
 setup(
     name='envs',
@@ -12,13 +15,13 @@ setup(
     license='GNU GPL v3',
     keywords='environment variables',
     extras_require={
-        'cli': [str(ir.req) for ir in cli_reqs],
+        'cli': parse_requirements('requirements_cli.txt'),
     },
     packages=find_packages(),
     py_modules=['envs.cli'],
     include_package_data=True,
     zip_safe=True,
-    version='1.2.4',
+    version='1.2.5',
     entry_points='''
         [console_scripts]
         envs=envs.cli:envs
